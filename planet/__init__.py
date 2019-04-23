08c465711c9f864bbc7d83f0a440c14623c7ae55
JingbinLiu <supernova.bin@gmail.com>
2019-04-23 15:56:05 +0800

    randcomm

--- planet/__init__.py
+++ planet/__init__.py
@@ -134,6 +134,77 @@ from __future__ import print_function
 # SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV' #  'TOWN2_WEATHER_NPC'  #
 # LOGDIR = '~/Data/planet/carla_64_reward0.0'
 
+
+
+#############################################
+
+# TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False  # use ENABLE_EXPERT to collect expert data.
+# IMG_SIZE = (64, 64)
+# H_SIZE, S_SIZE = 400, 60
+# EPISODE_LEN, COLLECT_EPISODE = 100, 300000.0   # collect data when accumulative reward < COLLECT_EPISODE.
+# REPEATE, NUM_SEED = 1, 300
+# BATCHSIZE, CHUNK_LEN = 50, 50
+# REWARD_FUNC = 'custom4'
+# USE_SENSOR = 'use_rgb'
+# NUM_CHANNELS = 3
+# SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV'
+# LOGDIR = '~/Data/planet/auto_carla_h-b_400-60'
+
+# TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False
+# IMG_SIZE = (128, 128)
+# H_SIZE, S_SIZE = 400, 60
+# EPISODE_LEN, COLLECT_EPISODE = 200, 300000.0
+# REPEATE, NUM_SEED = 1, 5
+# BATCHSIZE, CHUNK_LEN = 30, 30
+# REWARD_FUNC = 'custom3'
+# USE_SENSOR = 'use_rgb'
+# NUM_CHANNELS = 3
+# SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV' # 'TOWN2_NPC'  #  'TOWN2_WEATHER_NPC'  #
+# LOGDIR = '~/Data/planet/carla_128_400-60_hb_200'
+
+##########################################
+
+# TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False
+# IMG_SIZE = (64, 64)
+# H_SIZE, S_SIZE = 200, 30
+# EPISODE_LEN, COLLECT_EPISODE = 400, 300000.0
+# REPEATE, NUM_SEED = 1, 5
+# BATCHSIZE, CHUNK_LEN = 50, 50
+# REWARD_FUNC = 'custom3'
+# USE_SENSOR = 'use_rgb'
+# NUM_CHANNELS = 3
+# SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV' # 'TOWN2_NPC'  #  'TOWN2_WEATHER_NPC'  #
+# LOGDIR = '~/Data/planet/carla_64_200'
+
+##########################################
+
+TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False
+IMG_SIZE = (64, 64)
+H_SIZE, S_SIZE = 200, 30
+EPISODE_LEN, COLLECT_EPISODE = 600, 500.0
+REPEATE, NUM_SEED = 1, 5
+BATCHSIZE, CHUNK_LEN = 50, 50
+REWARD_FUNC = 'custom3'
+USE_SENSOR = 'use_rgb'
+NUM_CHANNELS = 3
+SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV' # 'TOWN2_NPC'  #  'TOWN2_WEATHER_NPC'  #
+LOGDIR = '~/Data/planet/carla_64_400'
+
+#############################################
+#
+# TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False
+# IMG_SIZE = (64, 64)
+# H_SIZE, S_SIZE = 400, 60
+# EPISODE_LEN, COLLECT_EPISODE = 600, 300000.0
+# REPEATE, NUM_SEED = 1, 5
+# BATCHSIZE, CHUNK_LEN = 50, 50
+# REWARD_FUNC = 'custom3'
+# USE_SENSOR = 'use_rgb'
+# NUM_CHANNELS = 3
+# SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV' # 'TOWN2_NPC'  #  'TOWN2_WEATHER_NPC'  #
+# LOGDIR = '~/Data/planet/carla_64_200_400-60'
+
+
 ##########################################
 
 # TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False
@@ -148,72 +219,7 @@ from __future__ import print_function
 # SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV' # 'TOWN2_NPC'  #  'TOWN2_WEATHER_NPC'  #
 # LOGDIR = '~/Data/planet/carla_128_200'
 
-##########################################
-
-# TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False
-# IMG_SIZE = (64, 64)
-# H_SIZE, S_SIZE = 200, 30
-# EPISODE_LEN, COLLECT_EPISODE = 200, 300000.0
-# REPEATE, NUM_SEED = 1, 5
-# BATCHSIZE, CHUNK_LEN = 50, 50
-# REWARD_FUNC = 'custom3'
-# USE_SENSOR = 'use_rgb'
-# NUM_CHANNELS = 3
-# SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV' # 'TOWN2_NPC'  #  'TOWN2_WEATHER_NPC'  #
-# LOGDIR = '~/Data/planet/carla_64_100'
-
-# TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False
-# IMG_SIZE = (64, 64)
-# H_SIZE, S_SIZE = 200, 30
-# EPISODE_LEN, COLLECT_EPISODE = 300, 300000.0
-# REPEATE, NUM_SEED = 1, 5
-# BATCHSIZE, CHUNK_LEN = 50, 50
-# REWARD_FUNC = 'custom3'
-# USE_SENSOR = 'use_rgb'
-# NUM_CHANNELS = 3
-# SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV' # 'TOWN2_NPC'  #  'TOWN2_WEATHER_NPC'  #
-# LOGDIR = '~/Data/planet/carla_64_200'
-#
-# # #
-# TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False
-# IMG_SIZE = (64, 64)
-# H_SIZE, S_SIZE = 200, 30
-# EPISODE_LEN, COLLECT_EPISODE = 600, 300000.0
-# REPEATE, NUM_SEED = 1, 5
-# BATCHSIZE, CHUNK_LEN = 50, 50
-# REWARD_FUNC = 'custom3'
-# USE_SENSOR = 'use_rgb'
-# NUM_CHANNELS = 3
-# SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV' # 'TOWN2_NPC'  #  'TOWN2_WEATHER_NPC'  #
-# LOGDIR = '~/Data/planet/carla_64_400'
-
 #############################################
-#
-TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False
-IMG_SIZE = (64, 64)
-H_SIZE, S_SIZE = 400, 60
-EPISODE_LEN, COLLECT_EPISODE = 600, 300000.0
-REPEATE, NUM_SEED = 1, 5
-BATCHSIZE, CHUNK_LEN = 50, 50
-REWARD_FUNC = 'custom3'
-USE_SENSOR = 'use_rgb'
-NUM_CHANNELS = 3
-SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV' # 'TOWN2_NPC'  #  'TOWN2_WEATHER_NPC'  #
-LOGDIR = '~/Data/planet/carla_64_200_400-60'
-
-#############################################
-
-# TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False
-# IMG_SIZE = (64, 64)
-# H_SIZE, S_SIZE = 400, 60
-# EPISODE_LEN, COLLECT_EPISODE = 500, 300000.0   # collect data when accumulative reward < COLLECT_EPISODE.
-# REPEATE, NUM_SEED = 1, 300
-# BATCHSIZE, CHUNK_LEN = 50, 50
-# REWARD_FUNC = 'custom4'
-# USE_SENSOR = 'use_rgb'
-# NUM_CHANNELS = 3
-# SCENARIO = 'TOWN2_ONE_CURVE_STRAIGHT_NAV'
-# LOGDIR = '~/Data/planet/auto_carla_h-b_400-60'
 
 
 # TASK_NAME = "{tasks: [carla]}"; ENABLE_EXPERT = False
